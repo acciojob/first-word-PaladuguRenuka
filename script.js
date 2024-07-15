ce051135170edc3478854e0db6b3e48d18283059
PaladuguRenuka <166904796+PaladuguRenuka@users.noreply.github.com>
2024-07-15 19:10:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 function firstWord(s) {
   // your code here
-	s = s.split(' ')
- let space = ' '
- if (s[0] === space && s.length > 1) {
-   return s[1]
-   //console.log(s[1])
- }
- else{
-   return s[0]
-   //console.log(s[0])
- }
+	 s = s.trim().split(' ');
+	return s[0]
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
